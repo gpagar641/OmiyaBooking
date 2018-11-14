906bd5d2b125ab382ca2a71015d5d8599636494b
BIONISCHE <BIONISCHE@DESKTOP-JDU67KT>
2018-11-14 23:49:40 +0530

    Ganesh

---
+++ src/main/java/com/maverick/demo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.maverick.demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OmiyaBookingApplication.class);
+	}
+
+}

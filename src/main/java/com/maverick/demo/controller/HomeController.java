e02093eba86d5a074f97658a85dc50a70292417f
BIONISCHE <BIONISCHE@DESKTOP-JDU67KT>
2018-11-15 01:16:09 +0530

    //Ganesh

---
+++ src/main/java/com/maverick/demo/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.maverick.demo.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	
+
+	@RequestMapping(value="/showHome", method=RequestMethod.GET)
+	public ModelAndView showHome(HttpServletRequest request)   
+	{
+		ModelAndView model=new ModelAndView("home");
+		
+		
+		
+		return model;
+		
+	}	
+	
+	
+}
